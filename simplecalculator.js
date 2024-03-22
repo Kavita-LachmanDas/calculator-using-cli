1692d2f62c04beee4ee5565bd41d605febb8f708
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-03-22 13:52:01 +0500

    Add files via upload

---
+++ simplecalculator.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        msg: "write your first number",
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        msg: "write your second number",
+        type: "number",
+        name: "secondNumber"
+    },
+    {
+        msg: "select your operator",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division", "exponential", "modulus"]
+    },]);
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiple") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "modulus") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operator === "exponential") {
+    console.log(answer.firstNumber ** answer.secondNumber);
+}
